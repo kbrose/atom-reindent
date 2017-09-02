02edbacc7996b14226afc46114070bd4d9e3c64a
Kevin Rose <kevin+gh@maypark.com>
2017-09-02 10:37:09 -0500

    reformat to be able to test multiple languages

--- spec/reindent-spec.js
+++ spec/reindent-spec.js
@@ -3,41 +3,37 @@ import Reindent from "../lib/reindent";
 
 
 describe("reindent", () => {
-    const FILE_NAME = "fixture.py";
     let buffer = null;
     let editor = null;
     let editorElement = null;
     let reindent = null;
 
     beforeEach(() => {
-        waitsForPromise(() =>
-            atom.workspace.open(FILE_NAME).then((ed) => {
-                editorElement = atom.views.getView(ed);
-                editor = ed;
-                editor.setSoftTabs(true);
-                editor.setTabLength(4);
-                buffer = editor.buffer;
-            }),
-        );
+        waitsForPromise(() => {
+            return atom.packages.activatePackage("language-python");
+        });
 
         waitsForPromise(() => {
-            const packages = atom.packages.getAvailablePackageNames();
-            let languagePackage;
-
-            if (packages.indexOf("language-python") > -1) {
-                languagePackage = "language-python";
-            } else if (packages.indexOf("MagicPython") > -1) {
-                languagePackage = "MagicPython";
-            }
-
-            return atom.packages.activatePackage(languagePackage);
+            return atom.packages.activatePackage("language-javascript");
         });
 
         reindent = new Reindent();
     });
 
-    // Aligned with opening delimiter
-    describe("reindents python files", () => {
+    describe("reindents python files (non-bracketed language)", () => {
+
+        beforeEach(() => {
+            waitsForPromise(() =>
+                atom.workspace.open("fixture.py").then((ed) => {
+                    editorElement = atom.views.getView(ed);
+                    editor = ed;
+                    editor.setSoftTabs(true);
+                    editor.setTabLength(4);
+                    buffer = editor.buffer;
+                }),
+            );
+        });
+
         describe("indents with colons", () => {
             /*
             for x in y:
@@ -59,6 +55,52 @@ describe("reindent", () => {
                 editor.selectAll();
                 reindent.reindent();
                 expect(buffer.lineForRow(1)).toBe(" ".repeat(4) + "f.read()");
+            });
+        });
+    });
+
+    describe("reindents javascript files (bracketed language)", () => {
+
+        beforeEach(() => {
+            waitsForPromise(() =>
+                atom.workspace.open("fixture.js").then((ed) => {
+                    editorElement = atom.views.getView(ed);
+                    editor = ed;
+                    editor.setSoftTabs(true);
+                    editor.setTabLength(4);
+                    buffer = editor.buffer;
+                }),
+            );
+        });
+
+        describe("indents with colons", () => {
+            /*
+            if (test) {
+                doSomething();
+            }
+            */
+            it("reindents an if statement", () => {
+                editor.insertText("if (test) {\n   doSomething();\n }\n");
+                editor.selectAll();
+                reindent.reindent();
+                expect(buffer.lineForRow(1)).toBe(" ".repeat(4) + "doSomething();");
+                expect(buffer.lineForRow(2)).toBe("}");
+            });
+
+            /*
+            for (let i = 0; i < n; i += 1) {
+               console.log(i)
+            }
+             noIndentHere();
+            */
+            it("reindents after closing bracket", () => {
+                editor.insertText(("for (let i = 0; i < n; i += 1) {\n"
+                                   + "   console.log(i)\n"
+                                   + "}\n"
+                                   + " noIndentHere();\n"));
+                editor.selectAll();
+                reindent.reindent();
+                expect(buffer.lineForRow(3)).toBe("noIndentHere();");
             });
         });
     });
